eaf44be8b40769e1fef599424b9a687e986909d3
Sergey Kogan <sekogan@gmail.com>
2021-11-23 23:55:39 +0300

    Add home row mods

--- kbd75hs/keymap/keymap.c
+++ kbd75hs/keymap/keymap.c
@@ -24,13 +24,24 @@ enum layer_id {
 #define CAPS_FN     LT(SECOND_LAYER, KC_CAPSLOCK)
 #define ESC_FN2     LT(THIRD_LAYER, KC_ESCAPE)
 
+#define LSFT_A      LSFT_T(KC_A)
+#define LCTL_S      LCTL_T(KC_S)
+#define LALT_D      LALT_T(KC_D)
+#define LGUI_F      LGUI_T(KC_F)
+
+#define RGUI_J      RGUI_T(KC_J)
+#define RALT_K      RALT_T(KC_K)
+#define RCTL_L      RCTL_T(KC_L)
+#define RSFT_SCLN   RSFT_T(KC_SCLN)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE_LAYER] = LAYOUT_75_ansi(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_INS,   KC_DEL,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,             KC_BSPC,  KC_HOME,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_END,
-    CAPS_FN,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGUP,
+    CAPS_FN,  LSFT_A,   LCTL_S,   LALT_D,   LGUI_F,   KC_G,     KC_H,     RGUI_J,   RALT_K,   RCTL_L,   RSFT_SCLN,KC_QUOT,                      KC_ENT,   KC_PGUP,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,    KC_PGDN,
     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
