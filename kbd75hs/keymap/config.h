02a6c6c41a712465360b785e2dec3b5806551c79
Sergey Kogan <sekogan@gmail.com>
2021-11-25 00:37:39 +0300

    Disable PERMISSIVE_HOLD

--- kbd75hs/keymap/config.h
+++ kbd75hs/keymap/config.h
@@ -2,6 +2,5 @@
 
 // https://precondition.github.io/home-row-mods#how-to-use-home-row-mods
 #define IGNORE_MOD_TAP_INTERRUPT
-#define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM        200
