3c35445776b0f3a5622b3a337f836aa600035a0f
Sergey Kogan <sekogan@gmail.com>
2021-11-27 03:35:19 +0300

    Remove unused code

--- sofle/keymap/keymap.c
+++ sofle/keymap/keymap.c
@@ -13,7 +13,6 @@ enum custom_keycodes {
     KC_COLEMK,
     KC_LOWER,
     KC_RAISE,
-    KC_ADJUST,
     KC_PRVWD,
     KC_NXTWD,
     KC_LSTRT,
@@ -232,13 +231,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 layer_off(_RAISE);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case KC_ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
             }
             return false;
         case KC_PRVWD:
