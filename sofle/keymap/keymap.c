99ffe5ea45ff2e8488982f4e85e704ee85d5a4fe
Sergey Kogan <sekogan@gmail.com>
2021-11-29 18:36:23 +0300

    Move `~ = -

--- sofle/keymap/keymap.c
+++ sofle/keymap/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  !   |  @   |  #   |  $   |  %   |  ^   |                    |  &   |  *   |  (   |  )   |      | BSPC |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ` ~  | = +  |  +   | - _  |  _   |      |,------.    ,------.| ' "  |  "   | / ?  | \ |  |  |   |      |
+ * |  ~   | ` ~  |  +   | = +  |  _   | - _  |,------.    ,------.| ' "  |  "   | / ?  | \ |  |  |   |      |
  * |------+------+------+------+------+------|| ___  |    | ___  ||------+------+------+------+------+------|
  * | ___  |      |      |      |  {   |  }   |`------'    `------'|  [   |  ]   |  <   |  >   |      | ___  |
  * `-------------+------+------+------+-.------------.    ,------------.-+------+------+------+-------------'
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12, \
   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,                      KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  XXXXXXX,  KC_BSPC, \
-  KC_GRV,   KC_EQL,   KC_PLUS,  KC_MINS,  KC_UNDS,  XXXXXXX,                      KC_QUOT,  KC_DQUO,  KC_SLSH,  KC_BSLS,  KC_PIPE,  XXXXXXX, \
+  KC_TILD,  KC_GRV,   KC_PLUS,  KC_EQL,   KC_UNDS,  KC_MINS,                      KC_QUOT,  KC_DQUO,  KC_SLSH,  KC_BSLS,  KC_PIPE,  XXXXXXX, \
   _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LCBR,  KC_RCBR,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_LT,    KC_GT,    XXXXXXX,  _______, \
                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______\
 ),
