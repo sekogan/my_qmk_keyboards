c17151586e81bdf473c873b1ba9c5b546e4aede2
Sergey Kogan <sekogan@gmail.com>
2021-12-07 20:18:37 +0300

    Fix encoder resolution

--- sofle/keymap/config.h
+++ sofle/keymap/config.h
@@ -1,9 +1,7 @@
 #pragma once
 
-/* The way how "handedness" is decided (which half is which),
-see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
-*/
-
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
+
+#undef ENCODER_RESOLUTION
+#define ENCODER_RESOLUTION 4
