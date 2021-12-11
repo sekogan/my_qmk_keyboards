7ef55e363078af1d8022729d81527ff943424351
Sergey Kogan <sekogan@gmail.com>
2021-12-11 20:16:56 +0300

    Add multimedia keys to the left encoder

--- sofle/keymap/keymap.c
+++ sofle/keymap/keymap.c
@@ -339,11 +339,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef ENCODER_ENABLE
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
+    uint8_t mods = get_mods();
     if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
+        if (mods & MOD_MASK_CTRL) {
+            if (clockwise) {
+                tap_code(KC_MNXT);
+            } else {
+                tap_code(KC_MPRV);
+            }
         } else {
-            tap_code(KC_VOLD);
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
         }
     } else if (index == 1) {
         if (clockwise) {
