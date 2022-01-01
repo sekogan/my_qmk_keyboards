974a537de665b4a2eb3ad14d9dbeb07ccfbccb2e
Sergey Kogan <sekogan@gmail.com>
2022-01-02 00:14:34 +0300

    Fix clipboard shortcuts feature

--- sofle/keymap/features/clipboard_shortcuts.c
+++ sofle/keymap/features/clipboard_shortcuts.c
@@ -1,31 +1,31 @@
-#include "clipboard.h"
+#include "clipboard_shortcuts.h"
 
 bool process_clipboard_shortcuts(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
         case KC_COPY:
             if (record->event.pressed) {
-                add_mods(mod_config(MOD_BIT(KC_LCTL)));
+                register_mods(mod_config(MOD_BIT(KC_LCTL)));
                 register_code(KC_C);
             } else {
-                del_mods(mod_config(MOD_BIT(KC_LCTL)));
+                unregister_mods(mod_config(MOD_BIT(KC_LCTL)));
                 unregister_code(KC_C);
             }
             return false;
         case KC_PASTE:
             if (record->event.pressed) {
-                add_mods(mod_config(MOD_BIT(KC_LCTL)));
+                register_mods(mod_config(MOD_BIT(KC_LCTL)));
                 register_code(KC_V);
             } else {
-                del_mods(mod_config(MOD_BIT(KC_LCTL)));
+                unregister_mods(mod_config(MOD_BIT(KC_LCTL)));
                 unregister_code(KC_V);
             }
             return false;
         case KC_CUT:
             if (record->event.pressed) {
-                add_mods(mod_config(MOD_BIT(KC_LCTL)));
+                register_mods(mod_config(MOD_BIT(KC_LCTL)));
                 register_code(KC_X);
             } else {
-                del_mods(mod_config(MOD_BIT(KC_LCTL)));
+                unregister_mods(mod_config(MOD_BIT(KC_LCTL)));
                 unregister_code(KC_X);
             }
             return false;
