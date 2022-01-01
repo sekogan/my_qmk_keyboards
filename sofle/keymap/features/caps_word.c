e71ed49739299637525a06c33a788c96862e5a3c
Sergey Kogan <sekogan@gmail.com>
2022-01-01 19:49:12 +0300

    Fix indentation

--- sofle/keymap/features/caps_word.c
+++ sofle/keymap/features/caps_word.c
@@ -19,64 +19,63 @@
 #include "caps_word.h"
 
 bool process_caps_word(uint16_t keycode, keyrecord_t* record) {
-  static bool caps_word_enabled = false;
-  static bool shifted = false;
+    static bool caps_word_enabled = false;
+    static bool shifted = false;
 #ifndef NO_ACTION_ONESHOT
-  const uint8_t mods = get_mods() | get_oneshot_mods();
+    const uint8_t mods = get_mods() | get_oneshot_mods();
 #else
-  const uint8_t mods = get_mods();
+    const uint8_t mods = get_mods();
 #endif  // NO_ACTION_ONESHOT
 
-  if (!caps_word_enabled) {
-    // Pressing both shift keys at the same time enables caps word.
-    if ((mods & MOD_MASK_SHIFT) == MOD_MASK_SHIFT) {
-      clear_mods();
+    if (!caps_word_enabled) {
+        // Pressing both shift keys at the same time enables caps word.
+        if ((mods & MOD_MASK_SHIFT) == MOD_MASK_SHIFT) {
+            clear_mods();
 #ifndef NO_ACTION_ONESHOT
-      clear_oneshot_mods();
+            clear_oneshot_mods();
 #endif  // NO_ACTION_ONESHOT
-      shifted = false;
-      caps_word_enabled = true;
-      return false;
+            shifted = false;
+            caps_word_enabled = true;
+            return false;
+        }
+        return true;
     }
+
+    if (!record->event.pressed) { return true; }
+
+    if (!(mods & ~MOD_MASK_SHIFT)) {
+        switch (keycode) {
+            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+            case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+                // Earlier return if this has not been considered tapped yet.
+                if (record->tap.count == 0) { return true; }
+                // Get the base tapping keycode of a mod- or layer-tap key.
+                keycode &= 0xff;
+        }
+
+        switch (keycode) {
+            // Letter keys should be shifted.
+            case KC_A ... KC_Z:
+                if (!shifted) { register_code(KC_LSFT); }
+                shifted = true;
+                return true;
+
+            // Keycodes that continue caps word but shouldn't get shifted.
+            case KC_1 ... KC_0:
+            case KC_BSPC:
+            case KC_MINS:
+            case KC_UNDS:
+                if (shifted) { unregister_code(KC_LSFT); }
+                shifted = false;
+                return true;
+
+            // Any other keycode disables caps word.
+        }
+    }
+
+    // Disable caps word.
+    caps_word_enabled = false;
+    if (shifted) { unregister_code(KC_LSFT); }
+    shifted = false;
     return true;
-  }
-
-  if (!record->event.pressed) { return true; }
-
-  if (!(mods & ~MOD_MASK_SHIFT)) {
-    switch (keycode) {
-      case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-      case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-        // Earlier return if this has not been considered tapped yet.
-        if (record->tap.count == 0) { return true; }
-        // Get the base tapping keycode of a mod- or layer-tap key.
-        keycode &= 0xff;
-    }
-
-    switch (keycode) {
-      // Letter keys should be shifted.
-      case KC_A ... KC_Z:
-        if (!shifted) { register_code(KC_LSFT); }
-        shifted = true;
-        return true;
-
-      // Keycodes that continue caps word but shouldn't get shifted.
-      case KC_1 ... KC_0:
-      case KC_BSPC:
-      case KC_MINS:
-      case KC_UNDS:
-        if (shifted) { unregister_code(KC_LSFT); }
-        shifted = false;
-        return true;
-
-      // Any other keycode disables caps word.
-    }
-  }
-
-  // Disable caps word.
-  caps_word_enabled = false;
-  if (shifted) { unregister_code(KC_LSFT); }
-  shifted = false;
-  return true;
 }
-
